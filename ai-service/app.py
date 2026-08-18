fcdd9d052238ee31bd4464b345d18004c88ee9fe
Snehal <snehalmundhe208@gmail.com>
2026-08-18 18:32:01 +0530

    Error removed

--- ai-service/app.py
+++ ai-service/app.py
@@ -136,7 +136,7 @@ def ask_ai(req: QueryRequest):
         "confidence": 0.98,
         "timestamp": time.strftime("%Y-%m-%d %H:%M:%S", )
     }
-     if __name__ == "__main__":
+    if __name__ == "__main__":
         import uvicorn
         uvicorn.run(app, host="0.0.0.0", port=8000)
     
